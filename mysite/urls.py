7c88dc86122ce593d98146082c4fcab4ac77e4e7
Yurley Sanchez <ysanchez@lendingfront.com>
2025-07-09 20:09:42 -0500

    definir vistas estaticas de centros comerciales

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('florida', TemplateView.as_view(template_name='florida.html'), name='florida'),
     path('admin/', admin.site.urls),
 ]
