bf417d918b371544fa3ca25b95d75a498e010c39
Yurley Sanchez <ysanchez@lendingfront.com>
2025-07-10 20:32:02 -0500

    creamos modelos de centros comerciales

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CentroComercial, Tienda
+
+@admin.register(CentroComercial)
+class CentroComercialAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'direccion', 'ciudad', 'telefono')
+    search_fields = ('nombre', 'direccion') 
+
+@admin.register(Tienda)
+class TiendaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'centro_comercial')
+    search_fields = ('nombre', 'direccion')
+    list_filter = ('centro_comercial',)
+    raw_id_fields = ('centro_comercial',)
