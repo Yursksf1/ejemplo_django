4d1933c42ce59de07e653187d506d3aed85442e0
Yurley Sanchez <ysanchez@lendingfront.com>
2025-07-14 20:20:31 -0500

    agregamos vistas, llamamos al modelo y renderizamos en el template

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from myapp import views
 
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('florida', TemplateView.as_view(template_name='florida.html'), name='florida'),
     path('cacique', TemplateView.as_view(template_name='cacique.html'), name='cacique'),
+    path('centros_comerciales', views.lista_centros_comerciales, name='lista_centros_comerciales'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import CentroComercial
 
 # Create your views here.
+def lista_centros_comerciales(request):
+    centros_comerciales = CentroComercial.objects.all()
+    return render(request, 'lista_centros_comerciales.html', {'centros_comerciales': centros_comerciales})
