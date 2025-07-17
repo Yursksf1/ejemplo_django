21dfa2f6ffbade175d849ef5b81665ec2225b9e9
Yurley Sanchez <ysanchez@lendingfront.com>
2025-07-16 19:25:29 -0500

    vista de tiendas

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,8 +4,9 @@ from myapp import views
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('home', TemplateView.as_view(template_name='home.html'), name='home'),
     path('florida', TemplateView.as_view(template_name='florida.html'), name='florida'),
     path('cacique', TemplateView.as_view(template_name='cacique.html'), name='cacique'),
-    path('centros_comerciales', views.lista_centros_comerciales, name='lista_centros_comerciales'),
+    path('', views.lista_centros_comerciales, name='lista_centros_comerciales'),
+    path('tiendas', views.lista_centros_tiendas, name='lista_centros_tiendas'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from .models import CentroComercial
+from .models import CentroComercial, Tienda
 
 # Create your views here.
 def lista_centros_comerciales(request):
     centros_comerciales = CentroComercial.objects.all()
     return render(request, 'lista_centros_comerciales.html', {'centros_comerciales': centros_comerciales})
+
+def lista_centros_tiendas(request):
+    tiendas = Tienda.objects.all()
+    return render(request, 'lista_tiendas.html', {'tiendas': tiendas})
